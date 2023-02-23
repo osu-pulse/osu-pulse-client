523637ec8ee4166ab51cff418167e6e733723ff1
T1MON <derbindima5@gmail.com>
2023-02-23 19:42:07 +0300

    fixed build

--- src/core/components/TheIntroLoader.vue
+++ src/core/components/TheIntroLoader.vue
@@ -1,12 +1,6 @@
-<script setup lang="ts">
-import ScaleLoader from 'vue-spinner/src/ScaleLoader.vue'
-</script>
-
 <template>
   <div class="intro-loader-component">
     <img class="icon" alt="logo" src="../../shared/assets/logo.webp">
-
-    <ScaleLoader margin="4px" color="var(--color-osu)" />
   </div>
 </template>
 
@@ -14,10 +8,10 @@ import ScaleLoader from 'vue-spinner/src/ScaleLoader.vue'
 .intro-loader-component {
   --color-osu: #fa94b2;
   display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  gap: 30px;
   background: radial-gradient(circle, white 50%, var(--color-osu) 100%);
+
+  .icon {
+    margin: auto;
+  }
 }
 </style>
